efd49c30a6938917fffade29a67115cf66020436
Thomas Müller <mimmi20@live.de>
2017-01-25 20:06:52 +0100

    change 9. round

--- c.php
+++ c.php
@@ -19,17 +19,17 @@ $companies    = json_decode(file_get_contents('vendor/mimmi20/browser-detector/d
 $browserTypes = json_decode(file_get_contents('vendor/mimmi20/ua-browser-type/data/types.json'));
 $deviceTypes  = json_decode(file_get_contents('vendor/mimmi20/ua-device-type/data/types.json'));
 
-foreach (new \RecursiveIteratorIterator($iterator) as $file) {
-    /** @var $file \SplFileInfo */
-    if (!$file->isFile() || $file->getExtension() !== 'json') {
-        continue;
-    }
-
-    echo 'handling ', $file->getPathname(), ' ...', PHP_EOL;
-
-    $tests = json_decode(file_get_contents($file->getPathname()));
-
-    foreach ($tests as $key => $test) {
+//foreach (new \RecursiveIteratorIterator($iterator) as $file) {
+//    /** @var $file \SplFileInfo */
+//    if (!$file->isFile() || $file->getExtension() !== 'json') {
+//        continue;
+//    }
+//
+//    echo 'handling ', $file->getPathname(), ' ...', PHP_EOL;
+//
+//    $tests = json_decode(file_get_contents($file->getPathname()));
+//
+//    foreach ($tests as $key => $test) {
 //        foreach ($companies as $companyKey => $companyData) {
 //            if ($test->properties->RenderingEngine_Maker === $companyData->name) {
 //                $test->properties->RenderingEngine_Maker = $companyKey;
@@ -51,49 +51,49 @@ foreach (new \RecursiveIteratorIterator($iterator) as $file) {
 //                $test->properties->Device_Brand_Name = $companyKey;
 //            }
 //        }
-        $company = $test->properties->RenderingEngine_Maker;
-
-        if (!isset($companies->$company)) {
-            $companies->$company = new stdClass();
-            $companies->$company->name = $company;
-            $companies->$company->brandname = $company;
-        }
-
-
-        $company = $test->properties->Browser_Maker;
-
-        if (!isset($companies->$company)) {
-            $companies->$company = new stdClass();
-            $companies->$company->name = $company;
-            $companies->$company->brandname = $company;
-        }
-
-
-        $company = $test->properties->Platform_Maker;
-
-        if (!isset($companies->$company)) {
-            $companies->$company = new stdClass();
-            $companies->$company->name = $company;
-            $companies->$company->brandname = $company;
-        }
-
-
-        $company = $test->properties->Device_Maker;
-
-        if (!isset($companies->$company)) {
-            $companies->$company = new stdClass();
-            $companies->$company->name = $company;
-            $companies->$company->brandname = $company;
-        }
-
-
-        $company = $test->properties->Device_Brand_Name;
-
-        if (!isset($companies->$company)) {
-            $companies->$company = new stdClass();
-            $companies->$company->name = $company;
-            $companies->$company->brandname = $company;
-        }
+//        $company = $test->properties->RenderingEngine_Maker;
+//
+//        if (!isset($companies->$company)) {
+//            $companies->$company = new stdClass();
+//            $companies->$company->name = $company;
+//            $companies->$company->brandname = $company;
+//        }
+//
+//
+//        $company = $test->properties->Browser_Maker;
+//
+//        if (!isset($companies->$company)) {
+//            $companies->$company = new stdClass();
+//            $companies->$company->name = $company;
+//            $companies->$company->brandname = $company;
+//        }
+//
+//
+//        $company = $test->properties->Platform_Maker;
+//
+//        if (!isset($companies->$company)) {
+//            $companies->$company = new stdClass();
+//            $companies->$company->name = $company;
+//            $companies->$company->brandname = $company;
+//        }
+//
+//
+//        $company = $test->properties->Device_Maker;
+//
+//        if (!isset($companies->$company)) {
+//            $companies->$company = new stdClass();
+//            $companies->$company->name = $company;
+//            $companies->$company->brandname = $company;
+//        }
+//
+//
+//        $company = $test->properties->Device_Brand_Name;
+//
+//        if (!isset($companies->$company)) {
+//            $companies->$company = new stdClass();
+//            $companies->$company->name = $company;
+//            $companies->$company->brandname = $company;
+//        }
 
 //        foreach ($browserTypes as $typeKey => $typeDate) {
 //            if ($test->properties->Browser_Type === $typeDate->name) {
@@ -108,83 +108,83 @@ foreach (new \RecursiveIteratorIterator($iterator) as $file) {
 //                break;
 //            }
 //        }
-    }
+//    }
 
 //    file_put_contents(
 //        $file->getPathname(),
 //        json_encode($tests, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) . PHP_EOL
 //    );
-}
-
-file_put_contents(
-    'vendor/mimmi20/browser-detector/data/companies.json',
-    json_encode($companies, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) . PHP_EOL
-);
-
-//foreach (new \RecursiveIteratorIterator($iterator) as $file) {
-//    /** @var $file \SplFileInfo */
-//    if (!$file->isFile() || $file->getExtension() !== 'json') {
-//        continue;
-//    }
-//
-//    echo 'handling ',  $file->getPathname(), ' ...', PHP_EOL;
-//
-//    $tests          = json_decode(file_get_contents($file->getPathname()));
-//    $convertedTests = [];
-//
-//    foreach ($tests as $key => $test) {
-//        $request = (new \Wurfl\Request\GenericRequestFactory())->createRequestForUserAgent($test->ua);
-//
-//        $engine = new \UaResult\Engine\Engine(
-//            $test->properties->RenderingEngine_Name,
-//            (new \BrowserDetector\Loader\CompanyLoader($cache))->load($test->properties->RenderingEngine_Maker),
-//            (new \BrowserDetector\Version\VersionFactory())->set($test->properties->RenderingEngine_Version)
-//        );
-//
-//        $browser = new \UaResult\Browser\Browser(
-//            $test->properties->Browser_Name,
-//            (new \BrowserDetector\Loader\CompanyLoader($cache))->load($test->properties->Browser_Maker),
-//            (new \BrowserDetector\Version\VersionFactory())->set($test->properties->Browser_Version),
-//            $engine,
-//            (new \UaBrowserType\TypeLoader($cache))->load($test->properties->Browser_Type),
-//            $test->properties->Browser_Bits,
-//            false,
-//            false,
-//            $test->properties->Browser_Modus
-//        );
-//
-//        $platform = new \UaResult\Os\Os(
-//            $test->properties->Platform_Codename,
-//            $test->properties->Platform_Marketingname,
-//            (new \BrowserDetector\Loader\CompanyLoader($cache))->load($test->properties->Platform_Maker),
-//            (new \BrowserDetector\Version\VersionFactory())->set($test->properties->Platform_Version),
-//            $test->properties->Platform_Bits
-//        );
-//
-//        $device = new \UaResult\Device\Device(
-//            $test->properties->Device_Code_Name,
-//            $test->properties->Device_Name,
-//            (new \BrowserDetector\Loader\CompanyLoader($cache))->load($test->properties->Device_Maker),
-//            (new \BrowserDetector\Loader\CompanyLoader($cache))->load($test->properties->Device_Brand_Name),
-//            null,
-//            $platform,
-//            (new \UaDeviceType\TypeLoader($cache))->load($test->properties->Device_Type),
-//            $test->properties->Device_Pointing_Method,
-//            null,
-//            null,
-//            $test->properties->Device_Dual_Orientation
-//        );
-//
-//        $result = new \UaResult\Result\Result($request, $device, $platform, $browser, $engine);
-//
-//        $convertedTests[$key] = [
-//            'ua'     => $test->ua,
-//            'result' => $result->toArray(),
-//        ];
-//    }
-//
-//    file_put_contents(
-//        $file->getPathname(),
-//        json_encode($convertedTests, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) . PHP_EOL
-//    );
 //}
+//
+//file_put_contents(
+//    'vendor/mimmi20/browser-detector/data/companies.json',
+//    json_encode($companies, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) . PHP_EOL
+//);
+
+foreach (new \RecursiveIteratorIterator($iterator) as $file) {
+    /** @var $file \SplFileInfo */
+    if (!$file->isFile() || $file->getExtension() !== 'json') {
+        continue;
+    }
+
+    echo 'handling ',  $file->getPathname(), ' ...', PHP_EOL;
+
+    $tests          = json_decode(file_get_contents($file->getPathname()));
+    $convertedTests = [];
+
+    foreach ($tests as $key => $test) {
+        $request = (new \Wurfl\Request\GenericRequestFactory())->createRequestForUserAgent($test->ua);
+
+        $engine = new \UaResult\Engine\Engine(
+            $test->properties->RenderingEngine_Name,
+            (new \BrowserDetector\Loader\CompanyLoader($cache))->load($test->properties->RenderingEngine_Maker),
+            (new \BrowserDetector\Version\VersionFactory())->set($test->properties->RenderingEngine_Version)
+        );
+
+        $browser = new \UaResult\Browser\Browser(
+            $test->properties->Browser_Name,
+            (new \BrowserDetector\Loader\CompanyLoader($cache))->load($test->properties->Browser_Maker),
+            (new \BrowserDetector\Version\VersionFactory())->set($test->properties->Browser_Version),
+            $engine,
+            (new \UaBrowserType\TypeLoader($cache))->load($test->properties->Browser_Type),
+            $test->properties->Browser_Bits,
+            false,
+            false,
+            $test->properties->Browser_Modus
+        );
+
+        $platform = new \UaResult\Os\Os(
+            $test->properties->Platform_Codename,
+            $test->properties->Platform_Marketingname,
+            (new \BrowserDetector\Loader\CompanyLoader($cache))->load($test->properties->Platform_Maker),
+            (new \BrowserDetector\Version\VersionFactory())->set($test->properties->Platform_Version),
+            $test->properties->Platform_Bits
+        );
+
+        $device = new \UaResult\Device\Device(
+            $test->properties->Device_Code_Name,
+            $test->properties->Device_Name,
+            (new \BrowserDetector\Loader\CompanyLoader($cache))->load($test->properties->Device_Maker),
+            (new \BrowserDetector\Loader\CompanyLoader($cache))->load($test->properties->Device_Brand_Name),
+            null,
+            $platform,
+            (new \UaDeviceType\TypeLoader($cache))->load($test->properties->Device_Type),
+            $test->properties->Device_Pointing_Method,
+            null,
+            null,
+            $test->properties->Device_Dual_Orientation
+        );
+
+        $result = new \UaResult\Result\Result($request, $device, $platform, $browser, $engine);
+
+        $convertedTests[$key] = [
+            'ua'     => $test->ua,
+            'result' => $result->toArray(),
+        ];
+    }
+
+    file_put_contents(
+        $file->getPathname(),
+        json_encode($convertedTests, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) . PHP_EOL
+    );
+}

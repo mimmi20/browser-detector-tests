1816d44246b3d9b85b6790f23df9e5fb7e93ba62
Thomas Müller <mimmi20@live.de>
2017-03-06 21:21:59 +0100

    fix error

--- tests/UserAgentsTest.php
+++ tests/UserAgentsTest.php
@@ -93,7 +93,7 @@ abstract class UserAgentsTest extends \PHPUnit\Framework\TestCase
             }
         }
 
-        echo ' finished (', str_pad(number_format(microtime(true) - $start, 4), 8, ' ', STR_PAD_LEFT), ' sec., ', str_pad(count($data), 6, ' ', STR_PAD_LEFT), ' test', (count($data) !== 1 ? 's' : ''), ')', PHP_EOL;
+        echo ' finished (', str_pad(number_format(microtime(true) - $start, 4), 8, ' ', STR_PAD_LEFT), ' sec., ', str_pad((string) count($data), 6, ' ', STR_PAD_LEFT), ' test', (count($data) !== 1 ? 's' : ''), ')', PHP_EOL;
 
         return $data;
     }

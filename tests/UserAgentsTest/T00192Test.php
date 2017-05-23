73fa4d0e04bea972904a5177db877fec111d759a
Thomas Müller <thomas.mueller@geld.de>
2017-05-23 11:58:07 +0200

    add new tests

---
+++ tests/UserAgentsTest/T00192Test.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * This file is part of the browser-detector-tests package.
+ *
+ * Copyright (c) 2015-2017, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+namespace BrowserDetectorTest\UserAgentsTest;
+
+use BrowserDetectorTest\UserAgentsTest;
+use UaResult\Result\Result;
+
+/**
+ * Class UserAgentsTest
+ *
+ * @category   CompareTest
+ *
+ * @author     Thomas Mueller <mimmi20@live.de>
+ * @group      useragenttest
+ * @group      integration
+ * @group      00192
+ */
+class T00192Test extends UserAgentsTest
+{
+    /**
+     * @var string
+     */
+    protected $sourceDirectory = 'tests/issues/00192/';
+
+    /**
+     * @dataProvider userAgentDataProvider
+     *
+     * @param string                  $userAgent
+     * @param \UaResult\Result\Result $expectedResult
+     *
+     * @throws \Exception
+     * @group  integration
+     * @group  useragenttest
+     * @group  00192
+     */
+    public function testUserAgents($userAgent, Result $expectedResult)
+    {
+        parent::testUserAgents($userAgent, $expectedResult);
+    }
+}

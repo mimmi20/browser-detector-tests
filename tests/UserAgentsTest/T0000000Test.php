133d703656eb5923aa06e1b9466126ad2c889619
t.mueller <thomas.mueller@geld.de>
2018-03-15 08:31:18 +0100

    change tests

--- tests/UserAgentsTest/T0000000Test.php
+++ tests/UserAgentsTest/T0000000Test.php
@@ -17,7 +17,7 @@ use PHPUnit\Framework\TestCase;
 /**
  * Class T0000000Test
  *
- * has 0 tests
+ * has 1000 tests
  * this file was created/edited automatically, please do not edit it
  *
  * @group      0000000

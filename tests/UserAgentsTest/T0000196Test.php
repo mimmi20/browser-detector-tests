5a207ce38631962bacbeca190eb5584cc19a628e
t.mueller <thomas.mueller@geld.de>
2018-04-27 14:42:59 +0200

    rewrite tests

--- tests/UserAgentsTest/T0000196Test.php
+++ tests/UserAgentsTest/T0000196Test.php
@@ -17,7 +17,7 @@ use PHPUnit\Framework\TestCase;
 /**
  * Class T0000196Test
  *
- * has 611 tests
+ * has 1000 tests
  * this file was created/edited automatically, please do not edit it
  *
  * @group      0000196

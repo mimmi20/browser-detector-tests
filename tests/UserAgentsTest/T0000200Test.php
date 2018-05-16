85817fc460d90aa12ab5ea1c68c008041db17718
t.mueller <thomas.mueller@geld.de>
2018-05-16 09:03:53 +0200

    update tests

--- tests/UserAgentsTest/T0000200Test.php
+++ tests/UserAgentsTest/T0000200Test.php
@@ -17,7 +17,7 @@ use PHPUnit\Framework\TestCase;
 /**
  * Class T0000200Test
  *
- * has 66 tests
+ * has 1000 tests
  * this file was created/edited automatically, please do not edit it
  *
  * @group      0000200

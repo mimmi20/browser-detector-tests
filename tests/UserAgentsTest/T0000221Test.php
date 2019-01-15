e9407c12e9e38b3039d8260857f7e601d7075f29
t.mueller <thomas.mueller@geld.de>
2019-01-15 08:31:40 +0100

    rewrite tests

--- tests/UserAgentsTest/T0000221Test.php
+++ tests/UserAgentsTest/T0000221Test.php
@@ -17,7 +17,7 @@ use PHPUnit\Framework\TestCase;
 /**
  * Class T0000221Test
  *
- * has 997 tests
+ * has 1000 tests
  * this file was created/edited automatically, please do not edit it
  *
  * @group      0000221

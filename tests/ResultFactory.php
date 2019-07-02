0ef74a723bc9b132e4c395858bd3ca96916690ee
t.mueller <thomas.mueller@geld.de>
2019-07-02 09:41:24 +0200

    update dependencies

--- tests/ResultFactory.php
+++ tests/ResultFactory.php
@@ -51,6 +51,9 @@ final class ResultFactory
      * @param \Psr\Log\LoggerInterface $logger
      * @param array                    $data
      *
+     * @throws \BrowserDetector\Version\NotNumericException
+     * @throws \UnexpectedValueException
+     *
      * @return \UaResult\Result\Result
      */
     public function fromArray(LoggerInterface $logger, array $data): Result

cd28bd686e3fce9515715e74722db67b498aea07
t.mueller <thomas.mueller@geld.de>
2019-10-07 11:21:20 +0200

    rewrite tests

--- tests/UserAgentsTest/T0000227Test.php
+++ tests/UserAgentsTest/T0000227Test.php
@@ -17,7 +17,7 @@ use PHPUnit\Framework\TestCase;
 /**
  * Class T0000227Test
  *
- * has 767 tests
+ * has 1000 tests
  * this file was created/edited automatically, please do not edit it
  *
  * @group 0000227
---
+++ tests/UserAgentsTest/T0000229Test.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the browser-detector-tests package.
+ *
+ * Copyright (c) 2015-2019, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+namespace BrowserDetectorTest\UserAgentsTest;
+
+use BrowserDetectorTest\UserAgentsTestTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class T0000229Test
+ *
+ * has 297 tests
+ * this file was created/edited automatically, please do not edit it
+ *
+ * @group 0000229
+ */
+final class T0000229Test extends TestCase
+{
+    use UserAgentsTestTrait;
+
+    /**
+     * @var string[]
+     */
+    private $sourceDirectory = [
+        'tests/issues/0000229/',
+    ];
+}

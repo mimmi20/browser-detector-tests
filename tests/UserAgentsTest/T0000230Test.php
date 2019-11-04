1e0507614f26e255de76a7582e331ac7b955d5a8
t.mueller <thomas.mueller@geld.de>
2019-11-04 15:26:04 +0100

    add new tests

--- tests/UserAgentsTest/T0000230Test.php
+++ tests/UserAgentsTest/T0000230Test.php
@@ -17,7 +17,7 @@ use PHPUnit\Framework\TestCase;
 /**
  * Class T0000230Test
  *
- * has 35 tests
+ * has 452 tests
  * this file was created/edited automatically, please do not edit it
  *
  * @group 0000230
